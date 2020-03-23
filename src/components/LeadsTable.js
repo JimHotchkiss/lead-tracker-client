91e5140e2f9a9970d2e1a313d0cfd7848f0b9b17
github username <github email address>
2020-03-23 09:38:26 -0700

    Switch from Material UI Table to Bootstrap table.Handling data, if feel, will be easier.

--- src/components/LeadsTable.js
+++ src/components/LeadsTable.js
@@ -1,77 +1,75 @@
 import React from "react";
 import { Component } from "react";
-import MaterialTable from "material-table";
-
+import Table from "react-bootstrap/Table";
 class LeadsTable extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    const columns = () => {
-      const newArray = [];
-      if (this.props.leads) {
-        this.props.leads.map(lead => {
-          for (let [key, value] of Object.entries(lead.attributes)) {
-            if (key === lead.attributes.product) {
-              return newArray.push(key);
-            }
-          }
-        });
-      }
-      console.log(newArray);
-    };
-    const MultipleActions = () => {
-      return (
-        <MaterialTable
-          title="Leads"
-          columns={[
-            {
-              title: "Name",
-              field: "name"
-            },
-            { title: "Surname", field: "surname" },
-            { title: "Birth Year", field: "birthYear", type: "numeric" },
-            {
-              title: "Birth Place",
-              field: "birthCity",
-              lookup: { 34: "İstanbul", 63: "Şanlıurfa" }
-            }
-          ]}
-          data={[
-            {
-              name: "Mehmet",
-              surname: "Baran",
-              birthYear: 1987,
-              birthCity: 63
-            },
-            {
-              name: "Zerya Betül",
-              surname: "Baran",
-              birthYear: 2017,
-              birthCity: 34
-            }
-          ]}
-          actions={[
-            {
-              icon: "search",
-              tooltip: "View and make changes to this lead",
-              onClick: (event, rowData) => alert("You deleted " + rowData.name)
-            },
-            {
-              icon: "delete",
-              tooltip: "Remove lead",
-              onClick: (event, rowData) =>
-                alert("You want to delete " + rowData.name)
-            }
-          ]}
-        />
-      );
-    };
     return (
-      <div className="leads-table">
-        {columns()}
-        {MultipleActions()}
-      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Leads</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
     );
   }
 }
