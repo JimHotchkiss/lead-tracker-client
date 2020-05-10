259d25f1db0e17c68384c08b9368c1d2a1d52de9
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-05-10 14:09:22 -0700

    Adding small update.

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -4,6 +4,7 @@ import "./login.css";
 const Login = () => {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
   const handleUserNameChange = (e) => {
     setUserName(e.target.value);
@@ -12,24 +13,52 @@ const Login = () => {
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
   return (
-    <div className='container'>
+    <div className='container '>
       <form>
         <div className='userName-label'>
           <label>Username:</label>
         </div>
         <div className='userName-input'>
-          <input type='text' onChange={handleUserNameChange} />
+          <input
+            placeholder='Username'
+            type='text'
+            onChange={handleUserNameChange}
+          />
+        </div>
+        <div className='email-label'>
+          <label>Email:</label>
+          <div className='email-input'>
+            <input
+              placeholder='Email'
+              type='text'
+              onChange={handleEmailChange}
+            />
+          </div>
         </div>
         <div className='password-label'>
           <label>Password:</label>
         </div>
         <div className='password-input'>
-          <input type='password' onChange={handlePasswordChange} />
+          <input
+            placeholder='Password'
+            type='password'
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <div className='submit-div'>
+          <button type='button' className='btn btn-primary'>
+            Submit
+          </button>
         </div>
       </form>
       {userName}
       {password}
+      {email}
     </div>
   );
 };
