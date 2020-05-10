e2b5d8130352bc762689ed8e2e0046ca3c86f22a
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-05-10 11:34:18 -0700

    Make the input component a functional component, made it a controled component, and added some basic styling.

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,15 +1,35 @@
 import React, { useState } from "react";
+import "./login.css";
 
 const Login = () => {
-  const [value, setValue] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUserNameChange = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
   return (
-    <div className='container-fluid'>
+    <div className='container'>
       <form>
-        <div>
-          <label>Login</label>
+        <div className='userName-label'>
+          <label>Username:</label>
         </div>
-        <input placeholder='' />
+        <div className='userName-input'>
+          <input type='text' onChange={handleUserNameChange} />
+        </div>
+        <div className='password-label'>
+          <label>Password:</label>
+        </div>
+        <div className='password-input'>
+          <input type='password' onChange={handlePasswordChange} />
+        </div>
       </form>
+      {userName}
+      {password}
     </div>
   );
 };
