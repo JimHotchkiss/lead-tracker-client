3f4a6dd44db4d1c1449d91acd69c665d6fc783af
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-06-21 17:06:13 -0700

    Added edit form.

--- src/containers/leads/EditLeadForm.js
+++ src/containers/leads/EditLeadForm.js
@@ -27,6 +27,8 @@ class EditLeadForm extends Component {
   //     this.props.leadSubmit(leadData, this.props);
   //   };
   render() {
+    console.log(this.props.name);
+
     // added = {} for 'undefined'
     // const {
     //   description,
@@ -47,17 +49,32 @@ class EditLeadForm extends Component {
             <Form.Label>Product</Form.Label>
 
             <select className='form-control' name='product'>
-              <option value=''>Select</option>
-              <option value='Camera'>Camera</option>
+              <option value={this.props.showLead.product}>
+                {this.props.showLead.product}
+              </option>
+              {this.props.showLead.product === "Camera" ? null : (
+                <option value='Camera'>Camera</option>
+              )}
+              {this.props.showLead.product === "Monitor" ? null : (
+                <option value='Monitor'>Monitor</option>
+              )}
+              {this.props.showLead.product === "Digital Capture" ? null : (
+                <option value='Digital Capture'>Digital Capture</option>
+              )}
+              {this.props.showLead.product === "Insufflator" ? null : (
+                <option value='Insufflator'>Insufflator</option>
+              )}
+
+              {/* <option value='Camera'>Camera</option>
               <option value='Monitor'>Monitor</option>
               <option value='Digital Capture'>Digital Capture</option>
-              <option value='Insufflator'>Insufflator</option>
+              <option value='Insufflator'>Insufflator</option> */}
             </select>
           </Form.Group>
           <Form.Group controlId='exampleForm.ControlTextarea1'>
             <Form.Label>Lead Description</Form.Label>
             <Form.Control
-              //   value={description}
+              value={this.props.showLead.description}
               name='description'
               as='textarea'
               rows='3'
@@ -67,20 +84,45 @@ class EditLeadForm extends Component {
           <Form.Group controlId='exampleForm.ControlSelect1'>
             <Form.Label>Urgency</Form.Label>
             <select className='form-control' name='urgency'>
-              <option value=''>Select</option>
-              <option value='Low'>Low</option>
+              <option value={this.props.showLead.urgency}>
+                {this.props.showLead.urgency}
+              </option>
+              {this.props.showLead.urgency === "Low" ? null : (
+                <option value='Low'>Low</option>
+              )}
+              {this.props.showLead.urgency === "Medium" ? null : (
+                <option value='Medium'>Medium</option>
+              )}
+              {this.props.showLead.urgency === "High" ? null : (
+                <option value='High'>High</option>
+              )}
+              {/* <option value='Low'>Low</option>
               <option value='Medium'>Medium</option>
-              <option value='High'>High</option>
+              <option value='High'>High</option> */}
             </select>
           </Form.Group>
           <Form.Group controlId='exampleForm.ControlSelect1'>
             <Form.Label>Status</Form.Label>
             <select className='form-control' name='status'>
-              <option value=''>Select</option>
-              <option value='New'>New</option>
+              <option value={this.props.showLead.status}>
+                {this.props.showLead.status}
+              </option>
+              {this.props.showLead.status === "New" ? null : (
+                <option value='New'>New</option>
+              )}
+              {this.props.showLead.status === "Open" ? null : (
+                <option value='Open'>Open</option>
+              )}
+              {this.props.showLead.status === "Pending" ? null : (
+                <option value='Pending'>Pending</option>
+              )}
+              {this.props.showLead.status === "Closed" ? null : (
+                <option value='Closed'>Closed</option>
+              )}
+              {/* <option value='New'>New</option>
               <option value='Open'>Open</option>
               <option value='Pending'>Pending</option>
-              <option value='Closed'>Closed</option>
+              <option value='Closed'>Closed</option> */}
             </select>
           </Form.Group>
           <div className='form-lead-contact-title'>
@@ -92,7 +134,7 @@ class EditLeadForm extends Component {
               //   value={contact_name}
               name='contact_name'
               type='name'
-              placeholder='Enter name'
+              placeholder={this.props.name}
             />
           </Form.Group>
           <Form.Group controlId='formGroupEmail'>
@@ -101,7 +143,7 @@ class EditLeadForm extends Component {
               //   value={email}
               name='email'
               type='email'
-              placeholder='Enter email'
+              placeholder={this.props.email}
             />
           </Form.Group>
           <Form.Group controlId='formGroupPassword'>
@@ -110,7 +152,7 @@ class EditLeadForm extends Component {
               //   value={phone_number}
               name='phone_number'
               type='phone_number'
-              placeholder='Enter phone number'
+              placeholder={this.props.phone}
             />
           </Form.Group>
           <Button variant='primary' type='submit'>
@@ -123,9 +165,15 @@ class EditLeadForm extends Component {
 }
 
 const mapStateToProps = (state) => {
+  let contact = {};
+  if (state.showLead.contact ? (contact = state.showLead.contact) : null)
+    console.log(state.showLead.contact);
   return {
     leadFormData: state.leadInput,
-    contact: state.leadInput.contact_attributes,
+    showLead: state.showLead,
+    name: contact.contact_name,
+    email: contact.email,
+    phone: contact.phone_number,
   };
 };
 
